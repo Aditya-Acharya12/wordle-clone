908c3a7e6cd2d11d4bb2599e5e0d82214c3c27c2
Aditya Acharya <acharya.aditya.2005@gmail.com>
2025-01-10 18:28:01 +0530

    Connect with wordbank to check validity of entered word

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import "./App.css";
 import Grid from "./components/Grid";
 import Keyboard from "./components/Keyboard";
-import { createContext } from "react";
-import { gridDefault } from "./words";
+import { gridDefault, generateWordSet } from "./words";
 
 export const AppContext = createContext();
 // to access the state in other components, we need to wrap the parent component in the createContext function
@@ -11,8 +10,15 @@ export const AppContext = createContext();
 function App() {
     const [grid, setGrid] = useState(gridDefault);
     const [currAttempt, setCurrAttempt] = useState({attemptNo: 0, letterPos: 0});
+    const [wordSet, setWordSet] = useState(new Set());
     const correctWord = "RIGHT";
     //using createContext to pass the grid state to the lower components from the parent component
+
+    useEffect(() => {
+        generateWordSet().then((words) => {
+          setWordSet(words.wordSet);
+        });
+      }, []);
 
     const onSelectLetter = (keyVal) => {
         if(currAttempt.letterPos === 5) return;
@@ -24,7 +30,18 @@ function App() {
 
     const onEnter = () => {
         if(currAttempt.letterPos !== 5)return;
-        setCurrAttempt({attemptNo: currAttempt.attemptNo + 1, letterPos : 0});
+
+        let currWord = "";
+        grid[currAttempt.attemptNo].forEach((letter) => {
+            currWord += letter;
+        });
+
+        if(wordSet.has(currWord.toLowerCase())){
+            setCurrAttempt({attemptNo: currAttempt.attemptNo + 1, letterPos : 0});
+        }
+        else{
+            alert("Invalid word");
+        }
     }
 
     const onDelete = () => {
--- src/words.js
+++ src/words.js
@@ -1,3 +1,4 @@
+import wordBank from "./wordle-bank.txt";
 export const gridDefault = [
 ["", "", "", "", ""], 
 ["", "", "", "", ""],
@@ -5,3 +6,14 @@ export const gridDefault = [
 ["", "", "", "", ""], 
 ["", "", "", "", ""],
 ["", "", "", "", ""]];
+
+export const generateWordSet = async() => {
+    let wordSet;
+    await fetch(wordBank).then((response) => response.text()).then((result)=> {
+        const wordArr = result.split("\n");
+        wordSet = new Set(wordArr);
+    })
+    return { wordSet };
+};
+//we use set instead of an arry as for an array we have to traverse through the entire array to check if element 
+//is present or not O(n), but for a set it is O(1) operation
